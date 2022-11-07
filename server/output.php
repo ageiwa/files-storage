37165a019ae964958ca05813d92798995c8b7f9d
Vlad Ageysha <ander123fakk@gmail.com>
2022-11-07 15:44:48 +0300

    Add docx icon

--- server/output.php
+++ server/output.php
@@ -6,15 +6,19 @@ for ($i = 2; $i < count($filesInFolder); $i++) {
     $fileName = $filesInFolder[$i];
     $size = filesize('./imgs/' . $filesInFolder[$i]) / 1024 . ' КБайт';
     $extension = new SplFileInfo('./imgs/' . $filesInFolder[$i]);
+    $iconClass = '';
+
+    if ($extension->getExtension() === 'docx') {
+        $iconClass = 'docx';
+    }
 
     echo 
     '<div class="file-list__item">
 
-        <div class="item__icon"></div>
+        <div class="item__icon '. $iconClass .'"></div>
         <div class="item__desc">
             <p class="item__name">'. $fileName .'</p>
             <p class="item__size">'. $size .'</p>
-            <p>'. $extension->getExtension() .'</p>
         </div>
 
     </div>';
