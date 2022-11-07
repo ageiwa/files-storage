a17461f7b8dd0ced6549690f4c2d42fdd4464b3c
Vlad Ageysha <ander123fakk@gmail.com>
2022-11-07 16:09:50 +0300

    Add txt icon

--- server/output.php
+++ server/output.php
@@ -11,15 +11,21 @@ for ($i = 2; $i < count($filesInFolder); $i++) {
     if ($extension->getExtension() === 'docx') {
         $iconClass = 'docx';
     }
+    else if ($extension->getExtension() === 'txt') {
+        $iconClass = 'txt';
+    }
 
     echo 
     '<div class="file-list__item">
 
-        <div class="item__icon '. $iconClass .'"></div>
-        <div class="item__desc">
-            <p class="item__name">'. $fileName .'</p>
-            <p class="item__size">'. $size .'</p>
+        <div class="item__group">
+            <div class="item__icon '. $iconClass .'"></div>
+            <div class="item__desc">
+                <p class="item__name">'. $fileName .'</p>
+                <p class="item__size">'. $size .'</p>
+            </div>
         </div>
+        <button class="item__btn-delete"></button>
 
     </div>';
 }
